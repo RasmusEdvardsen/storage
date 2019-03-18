110759cd3d9501c30bb75470662d39eaad5af2f2
buller børge <edvardsen.rasmus@gmail.com>
2019-03-18 19:53:20 +0100

    it works?

--- src/homestorage/homeStorage.vue
+++ src/homestorage/homeStorage.vue
@@ -6,73 +6,73 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { Component } from "vue-property-decorator";
-import { Action, Getter } from "vuex-class";
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { Action, Getter } from 'vuex-class';
 
 /* azure storage */
-import { ContainerItem } from "@azure/storage-blob/typings/lib/generated/lib/models";
-import { IBlobsByContainer } from "@/homestorage/module/homeStorageState";
+import { ContainerItem } from '@azure/storage-blob/typings/lib/generated/lib/models';
+import { IBlobsByContainer } from '@/homestorage/module/homeStorageState';
 
-import IFolderStructure from "./models/IFolderStructure";
+import IFolderStructure from './models/IFolderStructure';
 
-import { treeStructure } from "./utils/treeStructure";
+import { treeStructure } from './utils/treeStructure';
 
-const namespace = "homeStorage";
+const namespace = 'homeStorage';
 
 @Component
 export default class HomeStorage extends Vue {
-  @Action("getContainers", { namespace })
+  @Action('getContainers', { namespace })
   public getContainers: any;
 
-  @Action("getBlobsByContainer", { namespace })
+  @Action('getBlobsByContainer', { namespace })
   public getBlobsByContainer: any;
 
-  @Getter("containers", { namespace })
+  @Getter('containers', { namespace })
   public containers!: ContainerItem[];
 
-  @Getter("blobsByContainer", { namespace })
+  @Getter('blobsByContainer', { namespace })
   public blobsByContainer!: IBlobsByContainer;
 
   public tree = [
     {
-      id: "one",
-      name: "one",
+      id: 'one',
+      name: 'one',
       children: [
         {
-          id: "two",
-          name: "two",
-          children: []
+          id: 'two',
+          name: 'two',
+          children: [],
         },
         {
-          id: "77",
-          name: "77",
-          children: []
-        }
-      ]
+          id: '77',
+          name: '77',
+          children: [],
+        },
+      ],
     },
     {
-      id: "3",
-      name: "3",
+      id: '3',
+      name: '3',
       children: [
         {
-          id: "4",
-          name: "4",
-          children: []
+          id: '4',
+          name: '4',
+          children: [],
         },
         {
-          id: "66",
-          name: "66",
-          children: [{}]
-        }
-      ]
-    }
+          id: '66',
+          name: '66',
+          children: [{}],
+        },
+      ],
+    },
   ];
 
   public async mounted() {
     await this.getContainers();
-    this.getBlobsByContainer("homestorage");
-    const strs: string[] = this.blobsByContainer.blobs.map(b => b.name);
+    this.getBlobsByContainer('homestorage');
+    const strs: string[] = this.blobsByContainer.blobs.map((b) => b.name);
     treeStructure(strs);
   }
 }
--- src/homestorage/utils/treeStructure.ts
+++ src/homestorage/utils/treeStructure.ts
@@ -1,52 +1,42 @@
-// ["aaa/bbb/maxresdefault.jpg", "aaa/ccc/maxresdefault(1).jpg", "aaa/ccc/maxresdefault.jpg", "ccc/bbb/LUNDBECK-logo-RGB.jpg"]
+// takes in children of root objcet.
+
+//  for each path
+    //  split on /
+    //  concat on top-level path
+    //  for each concat paths
+        //  rec(paths)
+
 export function treeStructure(paths: string[]) {
-    let filtered: string[][] = paths.reduce((acc, curr) => {
-        console.log(curr);
-        return [[""]]
-    }, [[""]]);
+    console.log(paths);
+    const filtered: any = paths.reduce((acc: any, curr: string) => {
+        if (curr.indexOf('/') !== -1) {
+            let pathArr = curr.split('/')
+            let firstPath = pathArr[0];
+            if (Object.keys(acc).includes(firstPath)) {
+                acc[firstPath].children.push(pathArr.slice(1).join('/'))
+            } else {
+                acc[firstPath] = {
+                    id: firstPath,
+                    name: firstPath,
+                    children: [
+                        pathArr.slice(1).join('/')
+                    ]
+                }
+            }
+        } else if (curr.indexOf('.') !== -1) {
+            acc['asd'] = curr;
+        }
+        return acc;
+    }, {});
+    for (const key in filtered) {
+        if (filtered.hasOwnProperty(key)) {
+            const child = filtered[key];
+            if (Object.keys(child).includes('children')) {
+                child['children'] = treeStructure(child['children']);
+            }
+        }
+    }
     console.log(filtered);
+    return filtered;
+    // console.log(filtered);
 }
-
-// // ["aaa/bbb/maxresdefault.jpg", "aaa/ccc/maxresdefault(1).jpg", "aaa/ccc/maxresdefault.jpg", "ccc/bbb/LUNDBECK-logo-RGB.jpg"]
-// export function treeStructure(paths: string[]) {
-//     console.log(paths);
-//     let tree = {
-//         // Represents the "root" directory, like in a filesystem.
-//         root: {
-//             absolute_path: '',
-//             files: [],
-//         },
-//     };
-
-//     // ["aaa", "bbb", "maxresdefault.jpg"]
-//     function buildTree(parts: string[]) {
-//         console.log(parts);
-//         let lastDir: string = 'root';
-//         let abs_path: string = '';
-
-//         // "aaa"
-//         parts.forEach((name: string) => {
-//             // It's a directory
-//             if (name.indexOf('.') === -1) {
-//                 lastDir = name;
-//                 abs_path += lastDir + '/';
-
-//                 if (!tree[name]) {
-//                     tree[name] = {
-//                         absolute_path: abs_path,
-//                         files: [],
-//                     };
-//                 }
-//             } else {
-//                 tree[lastDir].files.push(name);
-//             }
-//         });
-//     }
-
-//     paths.forEach(function(path, index, array) {
-//         buildTree(path.split('/'));
-//     });
-
-//     console.log(tree);
-//     return tree;
-// }
