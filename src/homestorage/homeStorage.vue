50ff2e00f589ef5838e589be0feddbddc733220a
buller børge <edvardsen.rasmus@gmail.com>
2019-03-18 17:15:26 +0100

    astsadt

--- src/homestorage/homeStorage.vue
+++ src/homestorage/homeStorage.vue
@@ -1,66 +1,80 @@
 <template>
   <v-card id="home-storage">
     <v-card-title class="indigo white--text headline">User Directory</v-card-title>
-    <v-treeview :items="blobsByContainer.blobs"></v-treeview>
+    <v-treeview :items="tree"></v-treeview>
   </v-card>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
-import { Action, Getter } from 'vuex-class';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
 
 /* azure storage */
-import { ContainerItem } from '@azure/storage-blob/typings/lib/generated/lib/models';
-import { IBlobsByContainer } from '@/homestorage/module/homeStorageState';
+import { ContainerItem } from "@azure/storage-blob/typings/lib/generated/lib/models";
+import { IBlobsByContainer } from "@/homestorage/module/homeStorageState";
 
-import IFolderStructure from './models/IFolderStructure';
+import IFolderStructure from "./models/IFolderStructure";
 
-import { treeStructure } from './utils/treeStructure';
+import { treeStructure } from "./utils/treeStructure";
 
-const namespace = 'homeStorage';
+const namespace = "homeStorage";
 
 @Component
 export default class HomeStorage extends Vue {
-  @Action('getContainers', { namespace })
+  @Action("getContainers", { namespace })
   public getContainers: any;
 
-  @Action('getBlobsByContainer', { namespace })
+  @Action("getBlobsByContainer", { namespace })
   public getBlobsByContainer: any;
 
-  @Getter('containers', { namespace })
+  @Getter("containers", { namespace })
   public containers!: ContainerItem[];
 
-  @Getter('blobsByContainer', { namespace })
+  @Getter("blobsByContainer", { namespace })
   public blobsByContainer!: IBlobsByContainer;
+
+  public tree = [
+    {
+      id: "one",
+      name: "one",
+      children: [
+        {
+          id: "two",
+          name: "two",
+          children: []
+        },
+        {
+          id: "77",
+          name: "77",
+          children: []
+        }
+      ]
+    },
+    {
+      id: "3",
+      name: "3",
+      children: [
+        {
+          id: "4",
+          name: "4",
+          children: []
+        },
+        {
+          id: "66",
+          name: "66",
+          children: [{}]
+        }
+      ]
+    }
+  ];
 
   public async mounted() {
     await this.getContainers();
-    this.getBlobsByContainer('homestorage');
-    const strs: string[] = this.blobsByContainer.blobs.map((b) => b.name);
+    this.getBlobsByContainer("homestorage");
+    const strs: string[] = this.blobsByContainer.blobs.map(b => b.name);
     treeStructure(strs);
   }
-
-  // public rec(strs: string[]): IFolderStructure[] {
-  //   let folders = strs.reduce((acc, curr) => {
-  //     if (typeof(curr) !== 'string') return curr;
-
-  //     let split = curr.split('/');
-  //     let folderStructure: IFolderStructure = {
-  //         id: split[0],
-  //         name: split[0],
-  //         children: [split.slice(1).join('/')]
-  //     }
-
-  //     let existing = acc.find(c => c.name === folderStructure.name);
-  //     if (!existing) acc.push(folderStructure);
-  //     else (existing.children as string[]).push(split.slice(1).join('/'));
-
-  //     return acc;
-  //   }, Array<IFolderStructure>())
-
-  //   return folders;
-  // }
 }
 </script>
 
--- src/homestorage/utils/treeStructure.ts
+++ src/homestorage/utils/treeStructure.ts
@@ -1,38 +1,52 @@
-export function treeStructure(paths) {
-    let tree = {
-        // Represents the "root" directory, like in a filesystem.
-        root: {
-            absolute_path: '',
-            files: [],
-        },
-    };
-
-    function buildTree(parts) {
-        let lastDir = 'root';
-        let abs_path = '';
-
-        parts.forEach(function(name) {
-            // It's a directory
-            if (name.indexOf('.') === -1) {
-                lastDir = name;
-                abs_path += lastDir + '/';
-
-                if (!tree[name]) {
-                    tree[name] = {
-                        absolute_path: abs_path,
-                        files: [],
-                    };
-                }
-            } else {
-                tree[lastDir].files.push(name);
-            }
-        });
-    }
-
-    paths.forEach(function(path, index, array) {
-        buildTree(path.split('/'));
-    });
-
-    console.log(tree);
-    return tree;
+// ["aaa/bbb/maxresdefault.jpg", "aaa/ccc/maxresdefault(1).jpg", "aaa/ccc/maxresdefault.jpg", "ccc/bbb/LUNDBECK-logo-RGB.jpg"]
+export function treeStructure(paths: string[]) {
+    let filtered: string[][] = paths.reduce((acc, curr) => {
+        console.log(curr);
+        return [[""]]
+    }, [[""]]);
+    console.log(filtered);
 }
+
+// // ["aaa/bbb/maxresdefault.jpg", "aaa/ccc/maxresdefault(1).jpg", "aaa/ccc/maxresdefault.jpg", "ccc/bbb/LUNDBECK-logo-RGB.jpg"]
+// export function treeStructure(paths: string[]) {
+//     console.log(paths);
+//     let tree = {
+//         // Represents the "root" directory, like in a filesystem.
+//         root: {
+//             absolute_path: '',
+//             files: [],
+//         },
+//     };
+
+//     // ["aaa", "bbb", "maxresdefault.jpg"]
+//     function buildTree(parts: string[]) {
+//         console.log(parts);
+//         let lastDir: string = 'root';
+//         let abs_path: string = '';
+
+//         // "aaa"
+//         parts.forEach((name: string) => {
+//             // It's a directory
+//             if (name.indexOf('.') === -1) {
+//                 lastDir = name;
+//                 abs_path += lastDir + '/';
+
+//                 if (!tree[name]) {
+//                     tree[name] = {
+//                         absolute_path: abs_path,
+//                         files: [],
+//                     };
+//                 }
+//             } else {
+//                 tree[lastDir].files.push(name);
+//             }
+//         });
+//     }
+
+//     paths.forEach(function(path, index, array) {
+//         buildTree(path.split('/'));
+//     });
+
+//     console.log(tree);
+//     return tree;
+// }
