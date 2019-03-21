ebd94a13fd14ddc0708b43e61c2664327b5ceead
buller børge <edvardsen.rasmus@gmail.com>
2019-03-21 16:02:07 +0100

    sastokengenerator can take in permissions that is desired now.

--- src/sas/getSasToken.ts
+++ src/sas/getSasToken.ts
@@ -2,8 +2,9 @@ import { get } from './../web/web';
 
 export default async function getSasToken(): Promise<string | number> {
     try {
-        const sasTokenUri = 'https://homestoragefunc.azurewebsites.net/api/SasTokenGenerator';
-        const sasCall = await get<{ token: string }>(sasTokenUri);
+        const sasTokenQueryParameters = "?p=read,write,list,add,delete,create";
+        const sasTokenUri = "https://homestoragefunc.azurewebsites.net/api/SasTokenGenerator";
+        const sasCall = await get<{ token: string }>(sasTokenUri + sasTokenQueryParameters);
         if (sasCall.statusCode !== 200 || !sasCall.body) { return sasCall.statusCode; }
         return sasCall.body.token;
     } catch (error) {
