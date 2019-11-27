41c2f1867d7cc9364d0a988f1b7c8135e96adde5
Rasmus Edvardsen <rasmus.edvardsen@infomedia.dk>
2019-11-27 15:15:40 +0100

    config cause im retarded

--- homeweb/src/auth/config.ts
+++ homeweb/src/auth/config.ts
@@ -5,7 +5,7 @@ export const config: Configuration = {
         clientId: "f45ee6d4-23dd-4ada-b42a-447352d9dd70",
         authority:
             "https://login.microsoftonline.com/0d584642-1f4f-47d2-9ae2-8c67fea8fa12",
-        redirectUri: "http://localhost:8080",
+        redirectUri: "https://anarae.azurewebsites.net/",
     },
     cache: {
         cacheLocation: "localStorage",
